562bab9667bb1d31818555612cd85b4dd3e442d3
johan-smc <johan_smc@hotmail.com>
2016-05-11 16:36:51 -0500

    Test-html and js

---
+++ Pruebas/miJS.js
@@ -0,0 +1,18 @@
+function print(){
+  //document.write(1+2);
+  window.alert("mal");
+  document.getElementById("test").innerHTML=2+3;
+  console.log("helpo");
+
+}
+
+function setup()
+{
+
+
+}
+function loop()
+{
+
+
+}
